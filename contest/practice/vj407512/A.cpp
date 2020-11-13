54cf7ed6eb092f68a59b3ed5dc1c0cfebc8e3eba
WenDavid552 <david.jianwei.wen@gmail.com>
2020-11-13 12:32:24 +0800

    Build the structres of directories for all contests

---
+++ contest/practice/vj407512/A.cpp
@@ -0,0 +1,21 @@
+//poj2373
+//dp + 单调队列
+# include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e3+10;
+struct range{
+	int s,t;
+} r[maxn];
+int n,l;
+int a,b;
+//f[i] = min{f[j]} + 1 where j < i
+int queue
+int main(){
+	scanf("%d%d",&n,&l);
+	scanf("%d%d",&a,&b);
+	for(int i=1;i<=n;++i){
+		scanf("%d%d",&r[i].s,&r[i].t);
+	}
+	
+	return 0;
+}
